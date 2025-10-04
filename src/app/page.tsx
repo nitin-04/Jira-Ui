ed624c908de114c9ee067533bdd32f2ecf0dc6f7
Nitin <nitinbahuguna1251@gmail.com>
2025-10-04 23:01:47 +0530

    initialize

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
+export default function Home() {
+  return (
+    <div className="flex gap-4">
+      <Input />
+      Type here
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button>Primary</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
+    </div>
+  );
+}
