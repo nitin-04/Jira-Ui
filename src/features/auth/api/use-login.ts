5d8cd556c1800da97cf2ffa9e09560ed2cc6cd99
Nitin <nitinbahuguna1251@gmail.com>
2025-10-13 21:24:19 +0530

    add use-login

--- src/features/auth/api/use-login.ts
+++ src/features/auth/api/use-login.ts
@@ -2,9 +2,18 @@ import { useMutation } from '@tanstack/react-query';
 import { InferRequestType, InferResponseType } from 'hono';
 
 import { client } from '@/lib/rpc';
-import { Infer } from 'zod';
 
-type ResponseType = InferResponseType<typeof client.api.auth.login>;
-type RequestType = InferRequestType<typeof client.api.auth.login>;
+type ResponseType = InferResponseType<(typeof client.api.auth.login)['$post']>;
+type RequestType = InferRequestType<
+  (typeof client.api.auth.login)['$post']
+>['json'];
 
-export type LoginResponse = Infer<ResponseType>;
+export const useLogin = () => {
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.auth.login['$post']({ json });
+      return await response.json();
+    },
+  });
+  return mutation;
+};
