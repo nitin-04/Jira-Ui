309a95061b06993227f5403c686674c3bff002aa
Nitin <nitinbahuguna1251@gmail.com>
2025-12-23 20:32:34 +0530

    feat: enhance task management with filters and loading states in TaskViewSwitcher

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -3,16 +3,40 @@
 import { DottedSeparator } from '@/components/dotted-separator';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { PlusIcon } from 'lucide-react';
+import { Loader, PlusIcon } from 'lucide-react';
 import { useCreateTaskModal } from '../hooks/use-create-task-modal';
+import { useWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id';
+import { useGetTasks } from '../api/use-get-tasks';
+import { useQueryState } from 'nuqs';
+import { DataFilters } from './data.filters';
+import { useTaskFilters } from '../hooks/use-task-filters';
 
 export const TaskViewSwitcher = () => {
+  const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
+
+  const [view, setView] = useQueryState('task-view', {
+    defaultValue: 'table',
+  });
+
+  const workspaceId = useWorkspaceId();
   const { open } = useCreateTaskModal();
 
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+    status,
+    assigneeId,
+    projectId,
+    dueDate,
+  });
+
   return (
-    <Tabs className="flex-1 w-full border rounded-lg">
+    <Tabs
+      defaultValue={view}
+      onValueChange={setView}
+      className="flex-1 w-full border rounded-lg"
+    >
       <div className="h-full flex flex-col overflow-auto p-4">
-        <div className="flex flex-col gap-y-2 lg:flex-row lg:justify-between ">
+        <div className="flex flex-col gap-y-2 lg:flex-row justify-between ">
           <TabsList className="w-full lg:w-auto">
             <TabsTrigger className="h-8 w-full lg:w-auto" value="table">
               Table
@@ -30,19 +54,25 @@ export const TaskViewSwitcher = () => {
           </Button>
         </div>
         <DottedSeparator className="my-4" />
-        Data Filters
+        <DataFilters />
         <DottedSeparator className="my-4" />
-        <>
-          <TabsContent value="table" className="mt-0">
-            Data Table
-          </TabsContent>
-          <TabsContent value="kanban" className="mt-0">
-            Data Kanban
-          </TabsContent>
-          <TabsContent value="calendar" className="mt-0">
-            Data Calendar
-          </TabsContent>
-        </>
+        {isLoadingTasks ? (
+          <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
+            <Loader className="size-5 animate-spin to-muted-foreground" />
+          </div>
+        ) : (
+          <>
+            <TabsContent value="table" className="mt-0">
+              Data Table
+            </TabsContent>
+            <TabsContent value="kanban" className="mt-0">
+              Data Kanban
+            </TabsContent>
+            <TabsContent value="calendar" className="mt-0">
+              Data Calendar
+            </TabsContent>
+          </>
+        )}
       </div>
     </Tabs>
   );
